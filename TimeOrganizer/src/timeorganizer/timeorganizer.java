b02fe3cb373b61ae13d035cdba2f88ba36b7acf8
MSaldana362 <msaldana362@gmail.com>
2014-11-02 14:27:37 -0800

    Found a way to print current time.
    It's a start.
    I found it at:
    http://www.tutorialspoint.com/java/java_date_time.htm

--- TimeOrganizer/src/timeorganizer/timeorganizer.java
+++ TimeOrganizer/src/timeorganizer/timeorganizer.java
@@ -1,10 +1,16 @@
-
 package timeorganizer;
+import java.util.*;
 
 public class timeorganizer {
 
     public static void main(String[] args) {
-
+//Prints current time   LINK http://www.tutorialspoint.com/java/java_date_time.htm
+        
+      GregorianCalendar gcalendar = new GregorianCalendar();
+        System.out.print("Time: ");
+      System.out.print(gcalendar.get(Calendar.HOUR) + ":");
+      System.out.print(gcalendar.get(Calendar.MINUTE) + ":");
+      System.out.println(gcalendar.get(Calendar.SECOND));
     }
     
 }
