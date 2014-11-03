b7e42ad547d6aff644000c42916be985a05a4521
MSaldana362 <msaldana362@gmail.com>
2014-11-02 16:18:42 -0800

    Difference of Time
    Added code that can conjure the difference of the current time and a
    hard-coded time. Does not work with AM or PM yet.

--- TimeOrganizer/src/timeorganizer/timeorganizer.java
+++ TimeOrganizer/src/timeorganizer/timeorganizer.java
@@ -4,13 +4,43 @@ import java.util.*;
 public class timeorganizer {
 
     public static void main(String[] args) {
-//Prints current time   LINK http://www.tutorialspoint.com/java/java_date_time.htm
+        GregorianCalendar gcalendar = new GregorianCalendar();
         
-      GregorianCalendar gcalendar = new GregorianCalendar();
-        System.out.print("Time: ");
-      System.out.print(gcalendar.get(Calendar.HOUR) + ":");
-      System.out.print(gcalendar.get(Calendar.MINUTE) + ":");
-      System.out.println(gcalendar.get(Calendar.SECOND));
+//Hardcoded hour and minutes to test time differences
+        int testHour = 5;
+        int testMinute = 45;
+        
+        int currentHour = gcalendar.get(Calendar.HOUR);
+        int currentMin = gcalendar.get(Calendar.MINUTE);
+      
+//Prints current time   LINK http://www.tutorialspoint.com/java/java_date_time.htm
+      System.out.print("Time: ");
+      System.out.print(currentHour + ":");
+      if (currentMin<10)
+      System.out.println("0"+currentMin);
+      else
+      System.out.println(currentMin);
+      
+//Printint difference of current time and hardcoded time
+      System.out.print("Time difference from now to " + testHour + ":" + testMinute + " : ");
+      if (testMinute<currentMin)
+      {
+          System.out.print((testHour - currentHour)-1 + ":");
+          int minResult = testMinute +(60-currentMin);
+          if (minResult<10)
+          System.out.println("0" + minResult);
+          else
+          System.out.println(minResult);
+      }
+      else
+      {
+          System.out.print(testHour-currentHour + ":");
+          int minResult = testMinute-currentMin;
+          if (minResult<10)
+          System.out.println("0" + minResult);
+          else
+          System.out.println(minResult);
+      }
+
     }
-    
 }
